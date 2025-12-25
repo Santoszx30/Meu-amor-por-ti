0cd179d1f59ca1103b3547764322678e831d86ff
Santoszx30 <victorzinsantos100@gmail.com>
2025-12-25 11:37:28 -0300

    Add function to calculate days from a specific date

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function mostrarAmor() {
   amor.style.display = "block";
   amor.scrollIntoView({ behavior: "smooth" });
 }
+function calcularDias() {
+  const inicio = new Date(2025, 10, 24); // Mês começa em 0 (10 = novembro)
+  const hoje = new Date();
+
+  const diferenca = hoje - inicio;
+  const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
+  document.getElementById("dias").textContent = dias;
+}
+
+calcularDias();
